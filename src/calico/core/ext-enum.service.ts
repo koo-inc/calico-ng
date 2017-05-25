e37de0dcd457da89f0a67d67b532959291a0812d
角 輔 <tasuku@freemind.co.jp>
2017-05-25 15:40:19 +0900

    ExtEnumServiceで複数件リクエストが飛ぶ問題に対処

--- src/calico/core/ext-enum.service.ts
+++ src/calico/core/ext-enum.service.ts
@@ -6,6 +6,15 @@ import { LocalStorageService } from "./local-storage.service";
 
 export const EXT_ENUM_SERVICE_CONFIG = new InjectionToken<ExtEnumServiceConfig>('ExtEnumServiceConfig');
 
+declare type RequestCache = {
+  key: string;
+  value: ExtEnum[];
+};
+
+let requestCache: {[key: string]: Promise<RequestCache>} = {};
+let keyBuffer: string[] = [];
+let resolverCache = {};
+
 @Injectable()
 export class ExtEnumService {
   private apiPath: string;
@@ -47,27 +56,54 @@ export class ExtEnumService {
   }
 
   values(...keys: string[]): Observable<ExtEnumData> {
-    if(keys == null || keys.length == 0){
-      return Observable.of({});
-    }
-    let keysForApi: string[] = [];
-    let cachedData: ExtEnumData = new ExtEnumData();
-    for(let key of keys){
-      let cache = this.localStorageService.restore(this.createCacheKey(key), true);
-      if(cache){
-        cachedData[key] = cache;
-      }else{
-        keysForApi.push(key);
-      }
-    }
-    return this.fromApi(keysForApi).do(data => {
-      for(let key in data){
-        this.localStorageService.store(this.createCacheKey(key), data[key], true);
-      }
-      for(let key in cachedData){
-        data[key] = cachedData[key];
-      }
+    let observable = keys.map((key:string) => this.getValueStream(key))
+      .reduce((a: Observable<RequestCache>, b: Observable<RequestCache>) => a.merge(b)) as Observable<RequestCache>;
+
+    return observable
+      .bufferCount(keys.length)
+      .map((list: {key: string, value: ExtEnum[]}[]) => {
+        let data = {};
+        for (let item of list) {
+          data[item.key] = item.value;
+        }
+        return data;
+      });
+  }
+
+  private getValueStream(key: string): Observable<RequestCache> {
+    let cache = this.localStorageService.restore(this.createCacheKey(key), true);
+    if (cache) return Observable.of({key: key, value: cache});
+
+    let request = requestCache[key];
+    if (request != null) return Observable.fromPromise(request);
+
+    keyBuffer.push(key);
+
+    request = new Promise<RequestCache>((resolve, reject) => {
+      resolverCache[key] = {resolve: resolve, reject: reject};
+      setTimeout(() => {
+        let keys = keyBuffer.clone();
+        keyBuffer.length = 0;
+        this.fromApi(keys).subscribe(
+          data => {
+            keys.forEach(k => {
+              this.localStorageService.store(this.createCacheKey(k), data[k], true)
+              let resolver = resolverCache[k];
+              delete resolverCache[k];
+              delete requestCache[k];
+              resolver.resolve({key: k, value: data[k]});
+            });
+          },
+          err => keys.forEach(k => {
+            let resolver = resolverCache[k];
+            delete resolverCache[k];
+            resolver.reject(k)
+          })
+        );
+      }, 100);
     });
+    requestCache[key] = request;
+    return Observable.fromPromise(request);
   }
 
   private fromApi(keys: string[]): Observable<ExtEnumData> {
